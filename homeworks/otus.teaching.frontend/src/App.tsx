92d4fa6cd694c4dde24e7be1fc3f6d45bbc04108
vasiliev-alexey <alex_v81@mail.ru>
2024-04-13 12:54:07 +0300

    hw05

---
+++ homeworks/otus.teaching.frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "./components/Auth/login/LoginForm";
+
+export default function App() {
+  return (
+    <main>
+      <p>Otus Simple  Frontend</p>
+      <LoginForm />
+    </main>
+  );
+}
