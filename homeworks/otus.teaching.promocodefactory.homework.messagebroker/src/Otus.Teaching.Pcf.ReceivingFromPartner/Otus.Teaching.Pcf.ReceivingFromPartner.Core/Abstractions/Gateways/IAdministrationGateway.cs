6ae2e7c2a1901c30d1464f75c1fe516e554b8932
vasiliev-alexey <alex_v81@mail.ru>
2024-06-12 13:20:22 +0300

    hw 10 init

---
+++ homeworks/otus.teaching.promocodefactory.homework.messagebroker/src/Otus.Teaching.Pcf.ReceivingFromPartner/Otus.Teaching.Pcf.ReceivingFromPartner.Core/Abstractions/Gateways/IAdministrationGateway.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Otus.Teaching.Pcf.ReceivingFromPartner.Core.Abstractions.Gateways
+{
+    public interface IAdministrationGateway
+    {
+        Task NotifyAdminAboutPartnerManagerPromoCode(Guid partnerManagerId);
+    }
+}
